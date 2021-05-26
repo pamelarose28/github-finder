b7e7eac1d50bbef02772060c46e04ac74ac1d6c5
Pamela Davenport <pamelarose28@gmail.com>
2021-05-26 11:39:45 -0400

    navbar component created with icon and title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React, {Component} from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
     <div className="App">
-      <h1> Hello from React</h1>
+    <Navbar title="Github Finder" icon="fab fa-github"/>
     </div>
   );
+  }
 }
 
 export default App;
