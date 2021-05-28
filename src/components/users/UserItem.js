4e484f5b2465fe787b24c71f2eb7ecd20b3b2680
Pamela Davenport <pamelarose28@gmail.com>
2021-05-28 14:23:27 -0400

    connected api from github

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-
-
-    render() {
-        const { login, avatar_url, html_url } = this.props.user;
+ const UserItem = ({user: {login, avatar_url, html_url}}) =>  {
 
         return (
             <div className='card text-center'>
@@ -14,8 +11,12 @@ class UserItem extends Component {
                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a> 
                 </div>
             </div>
-        )
-    }
+        );
+    
+};
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
 }
 
 export default UserItem
