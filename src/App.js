5c5bc3668abf48201bf692e6dd36276c94e54fcd
Pamela Davenport <pamelarose28@gmail.com>
2021-05-27 21:42:56 -0400

    added users cards into container below navbar, grid layout of cards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
 class App extends Component {
-  render() {
+
+  render(){
     return (
     <div className="App">
-    <Navbar title="Github Finder" icon="fab fa-github"/>
+     <Navbar title='Github Finder' icon='fab fa-github'/>
+     <div className='container'>
+      <Users />
+     </div>
+      
     </div>
   );
-  }
+    }
+  
 }
 
 export default App;
